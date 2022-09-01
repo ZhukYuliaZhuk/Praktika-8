336d564619b231589d03b278a259b63698846c32
Юлия Жук <sheburova801@mail.ru>
2022-09-01 22:37:50 +0300

    54

--- 54/Program.cs
+++ 54/Program.cs
@@ -1,11 +1,13 @@
-﻿//Задача 56: Задайте прямоугольный двумерный массив. 
-//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
 //8 4 2 4
-//5 2 6 7
-//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
 
 
 Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
@@ -17,27 +19,27 @@ int[,] array = new int[m, n];
 CreateArray(array);
 WriteArray(array);
 
-int minSumLine = 0;
-int sumLine = SumLineElements(array, 0);
-for (int i = 1; i < array.GetLength(0); i++)
-{
-  int tempSumLine = SumLineElements(array, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
 
-Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
-int SumLineElements(int[,] array, int i)
+void OrderArrayLines(int[,] array)
 {
-int sumLine = array[i,0];
-for (int j = 1; j < array.GetLength(1); j++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-sumLine += array[i,j];
+for (int j = 0; j < array.GetLength(1); j++)
+{
+for (int k = 0; k < array.GetLength(1) - 1; k++)
+{
+if (array[i, k] < array[i, k + 1])
+{
+int temp = array[i, k + 1];
+array[i, k + 1] = array[i, k];
+array[i, k] = temp;
 }
-return sumLine;
+}
+}
+}
 }
 
 int InputNumbers(string input)
@@ -58,13 +60,13 @@ array[i, j] = new Random().Next(range);
 }
 }
 
-void WriteArray (int[,] array)
+void WriteArray(int[,] array)
 {
 for (int i = 0; i < array.GetLength(0); i++)
 {
 for (int j = 0; j < array.GetLength(1); j++)
 {
-Console.Write(array[i,j] + " ");
+Console.Write(array[i, j] + " ");
 }
 Console.WriteLine();
 }
